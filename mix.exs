15ae60f8f62e06c694663652a98bbfa549e445b4
javierg <javierg@amcoonline.net>
2016-07-28 21:07:23 -0700

    push versions

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule CouchFactory.Mixfile do
 
   def project do
     [app: :couch_factory,
-     version: "0.1.1",
-     elixir: "~> 1.0",
+     version: "0.2.0",
+     elixir: "~> 1.1",
      description: description,
      package: package,
      deps: deps]
